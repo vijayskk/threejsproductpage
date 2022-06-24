dec8be6e7c742fe18cf1d7af105d78ca56bfa1cb
vijayskk <vijayskk007@gmail.com>
2022-06-24 13:01:57 +0530

    Responsiveness is yet to fix

--- main.js
+++ main.js
@@ -69,9 +69,9 @@ window.addEventListener( 'resize', onWindowResize, false );
 
 function onWindowResize(){
 
-    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = (window.innerWidth) / window.innerHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( (window.innerWidth), window.innerHeight );
 
 }
